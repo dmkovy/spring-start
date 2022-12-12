0c4a968004f9a4f1eb9df9ad6a7517211d1d250b
Dmitry Kovalchuk <andycoutur@gmail.com>
2022-12-12 23:31:48 +0200

    add init/destroy/factory method

--- src/main/java/ru/dmkovy/springstart/DubstepMusic.java
+++ src/main/java/ru/dmkovy/springstart/DubstepMusic.java
@@ -1,8 +1,20 @@
 package ru.dmkovy.springstart;
 
 public class DubstepMusic implements Music{
+    // Фабричный метод: приватный конструктор и паблик статик метод для создания нового объекта
+    private DubstepMusic() {}
+    public static DubstepMusic getDubstepMusic() {
+        return new DubstepMusic();
+    }
     @Override
     public String getSong() {
         return "Heavy Artillery Bass Song";
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing initialization");
+    }
+    public void doMyDestroy() {
+        System.out.println("Doing destruct");
     }
 }
--- src/main/java/ru/dmkovy/springstart/TestSpring.java
+++ src/main/java/ru/dmkovy/springstart/TestSpring.java
@@ -10,11 +10,32 @@ public class TestSpring {
 //        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
 
         // Использовали DI и теперь можем вызывать musicPlayer из контекста
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+
+//        MusicPlayer firstMP = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMP = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        System.out.println(firstMP == secondMP);
+//        System.out.println(firstMP.equals(secondMP));
+//
+//        System.out.println(firstMP);
+//        System.out.println(secondMP);
+//
+//        firstMP.setVolume(10);
+//        System.out.println(firstMP.getVolume());
+//        System.out.println(secondMP.getVolume());
+
+        DubstepMusic dubstepMusic1 = context.getBean("musicBean3", DubstepMusic.class);
+        DubstepMusic dubstepMusic2= context.getBean("musicBean3", DubstepMusic.class);
+        DubstepMusic dubstepMusic3 = context.getBean("musicBean3", DubstepMusic.class);
+
+        System.out.println(dubstepMusic1.getSong());
+        System.out.println(dubstepMusic2.getSong());
+        System.out.println(dubstepMusic3.getSong());
 
         context.close();
     }
